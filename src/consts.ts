e135287262fcdc17569c9ee06ae7f12bffd88023
Max <0xmaxyz@gmail.com>
2025-04-10 14:35:41 +0330

    add project consts file

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+export const BASE_REPO_URL =
+  "https://github.com/1delta-DAO/contracts-delegation/raw/refs/heads/composer-compact/";
+export const CALLDATA_LIB_PATH = "test/light/utils/CalldataLib.sol";
+export const CALLDATA_LIB_URL = `${BASE_REPO_URL}${CALLDATA_LIB_PATH}`;
+
+export const INPUT_DIR = "./data/input";
+export const OUTPUT_DIR = "./data/output";
