3ceef01df7819e81274e9061ce1bc736a3aeb2b3
Max <0xmaxyz@gmail.com>
2025-04-29 12:50:44 +0330

    Update consts.ts
    update branch to `main`

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 import path from "path";
 
 export const BASE_REPO_URL =
-  "https://github.com/1delta-DAO/contracts-delegation/raw/refs/heads/composer-compact/";
+  "https://github.com/1delta-DAO/contracts-delegation/raw/refs/heads/main/";
 export const CALLDATA_LIB_PATH = "test/composer/utils/CalldataLib.sol";
 export const CALLDATA_LIB_URL = `${BASE_REPO_URL}${CALLDATA_LIB_PATH}`;
 
